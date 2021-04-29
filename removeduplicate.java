b829b27d35637f7ff20d84a57a1fe9b558d11bdd
AZHARUDDIN SK <78663787+AzharJava@users.noreply.github.com>
2021-04-29 19:32:03 +0530

    Add files via upload

---
+++ removeduplicate.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class duplicateRemove{
+    //function to remove duplicate array.
+    //create user define function.
+    public static void removeDuplicate(int [] a){ 
+        LinkedHashSet<Integer> set=new LinkedHashSet<Integer>();  //scanner for create
+
+        //adding elements to LinkedHashSet.
+
+        for (int i=0; i<a.length; i++)
+            set.add(a[i]);
+
+        //print the element from LinkedHashSet
+        System.out.print(set);
+
+    }
+
+    //now create main function.
+    public static void main(String[] args) {
+        int a[] = {5, 2, 6, 8, 6, 7, 5, 2, 8};
+        //calling function
+        removeDuplicate(a);
+    }
+}
